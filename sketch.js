e6d7c6b77be7fb82d3dc2d77a9b6ae24465d5817
Advika005 <64787525+Advika005@users.noreply.github.com>
2020-08-05 16:42:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var hr;
+var mn;
+var sc;
+
+function setup(){
+  createCanvas(400,400);
+  angleMode(DEGREES);
+}
+  
+function draw(){
+  background(0);
+  translate(200,200)
+  
+  hr = hour();
+  mn = minute();
+  sc = second();
+  
+  
+}
